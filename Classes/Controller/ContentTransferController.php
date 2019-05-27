9b69c94d4c0b4c77a8774c1aa8884a99c9f2d237
Sebastian Helzle <sebastian@helzle.it>
2019-05-27 12:04:06 +0200

    TASK: Initial commit

---
+++ Classes/Controller/ContentTransferController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Shel\Neos\TransferContent\Controller;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Error\Messages\Message;
+use Neos\Flow\I18n\Translator;
+use Neos\Neos\Controller\Module\AbstractModuleController;
+use Neos\Neos\Domain\Model\Site;
+use Neos\Neos\Domain\Repository\SiteRepository;
+use Neos\Neos\Domain\Service\ContentContext;
+use Neos\Neos\Service\NodeOperations;
+use Neos\ContentRepository\Domain\Service\ContextFactory;
+use Neos\ContentRepository\Exception\NodeException;
+
+/**
+ * Controller
+ *
+ * @Flow\Scope("singleton")
+ */
+class ContentTransferController extends AbstractModuleController
+{
+
+    /**
+     * @var NodeOperations
+     * @Flow\Inject
+     */
+    protected $nodeOperations;
+
+    /**
+     * @Flow\Inject
+     * @var SiteRepository
+     */
+    protected $siteRepository;
+
+    /**
+     * @Flow\Inject
+     * @var ContextFactory
+     */
+    protected $contextFactory;
+
+    /**
+     * @Flow\Inject
+     * @var Translator
+     */
+    protected $translator;
+
+    /**
+     * Shows form to transfer content
+     * @param Site $sourceSite
+     * @param Site $targetSite
+     * @param string $targetParentNodePath
+     */
+    public function indexAction(Site $sourceSite = null, Site $targetSite = null, $targetParentNodePath = '')
+    {
+        $sites = $this->siteRepository->findOnline();
+
+        $this->view->assignMultiple([
+            'sites' => $sites,
+            'sourceSite' => $sourceSite,
+            'targetSite' => $targetSite,
+            'targetParentNodePath' => $targetParentNodePath,
+        ]);
+    }
+
+    /**
+     * @param Site $sourceSite
+     * @param Site $targetSite
+     * @param string $sourceNodePath
+     * @param string $targetParentNodePath
+     * @Flow\Validate(argumentName="sourceNodePath", type="\Neos\Flow\Validation\Validator\NotEmptyValidator")
+     * @Flow\Validate(argumentName="targetParentNodePath", type="\Neos\Flow\Validation\Validator\NotEmptyValidator")
+     * @throws \Neos\Flow\Mvc\Exception\StopActionException
+     */
+    public function copyNodeAction(Site $sourceSite, Site $targetSite, $sourceNodePath, $targetParentNodePath)
+    {
+        /** @var ContentContext $sourceContext */
+        $sourceContext = $this->contextFactory->create([
+            'currentSite' => $sourceSite,
+            'invisibleContentShown' => true,
+            'inaccessibleContentShown' => true
+        ]);
+
+        /** @var ContentContext $targetContext */
+        $targetContext = $this->contextFactory->create([
+            'currentSite' => $targetSite,
+            'invisibleContentShown' => true,
+            'inaccessibleContentShown' => true
+        ]);
+
+        $sourceNode = $sourceContext->getNodeByIdentifier($sourceNodePath);
+        $targetParentNode = $targetContext->getNodeByIdentifier($targetParentNodePath);
+
+        if ($sourceNode === null) {
+            $this->addFlashMessage(
+                $this->translate('error.sourceNodeNotFound'),
+                'Error',
+                Message::SEVERITY_ERROR
+            );
+        } else if ($targetParentNode === null) {
+            $this->addFlashMessage(
+                $this->translate('error.targetParentNodeNotFound'),
+                'Error',
+                Message::SEVERITY_ERROR
+            );
+        } else if (!$sourceNode->getNodeType()->isOfType('Neos.Neos:Document')) {
+            $this->addFlashMessage(
+                $this->translate('error.invalidSourceNode', [$sourceNode->getNodeType()]),
+                'Error',
+                Message::SEVERITY_ERROR
+            );
+        } else if (!$targetParentNode->getNodeType()->isOfType('Neos.Neos:Document')) {
+            $this->addFlashMessage(
+                $this->translate('error.invalidTargetParentNode', [$targetParentNode->getNodeType()]),
+                'Error',
+                Message::SEVERITY_ERROR
+            );
+        } else if (!$targetParentNode->isNodeTypeAllowedAsChildNode($sourceNode->getNodeType())) {
+            $this->addFlashMessage(
+                $this->translate('error.sourceNodeNotAllowedAsChildNode'),
+                'Error',
+                Message::SEVERITY_ERROR
+            );
+        } else {
+            try {
+                $this->nodeOperations->copy($sourceNode, $targetParentNode, 'into');
+                $this->addFlashMessage(
+                    $this->translate('message.copied'),
+                    'Success'
+                );
+            } catch (NodeException $e) {
+                $this->addFlashMessage(
+                    $this->translate('error.copyFailed', [$e->getReferenceCode()]),
+                    'Error',
+                    Message::SEVERITY_ERROR
+                );
+            }
+        }
+
+        $this->redirect('index', null, null, [
+            'sourceSite' => $sourceSite,
+            'targetSite' => $targetSite,
+            'targetParentNodePath' => $targetParentNodePath,
+        ]);
+    }
+
+    /**
+     * @param string $id
+     * @param array $arguments
+     * @return string
+     */
+    protected function translate($id, array $arguments = [])
+    {
+        return $this->translator->translateById($id, $arguments, null, null, 'ContentTransfer', 'Shel.Neos.TransferContent');
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Backend module for transferring content in Neos CMS multi sites
+
+## Introduction
+
+This plugin will add a new backend module for copying nodes between sites in a 
+Neos multi site installation.
+
+It's currently compatible with Neos 4.3.
+                
+## Installation
+
+Run this in your site package
+
+    composer require --no-update shel/neos-hyphens
+    
+Then run `composer update` in your project directory.
+
+## How to use
+
+TODO
+
+## Contributions
+
+Contributions are very welcome! 
+
+Please create detailed issues and PRs.  
+
+**If you use this package and want to support or speed up it's development, [get in touch with me](mailto:transfercontent@helzle.it).**
+
+Or you can also support me directly via [patreon](https://www.patreon.com/shelzle).
+
+## License
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
